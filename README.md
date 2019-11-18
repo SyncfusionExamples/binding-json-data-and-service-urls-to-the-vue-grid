7027b87690d810f6080c32d7c3ed9f109f20e802
Uma Maheswari Chandrabose <39375672+UmaMaheswariChandrabose@users.noreply.github.com>
2019-11-18 16:59:48 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# binding-json-data-and-service-urls-to-the-vue-grid
+# Binding JSON Data and Service URLs to the Vue Grid of Syncfusion
+
+A quick start project that helps you to bind the remote data to the Syncfusion Vue Grid and also to load data dynamically through the AJAX post. Please watch the below video to bind local JSON data to the Vue Grid.
+
+Watch the getting-started video now: https://www.syncfusion.com/tutorial-videos/vue/grid?title=how-to-get-started-with-the-syncfusion-vue-grid
+
+Documentation: https://ej2.syncfusion.com/vue/documentation/grid/data-binding/
+
+Online Example: https://ej2.syncfusion.com/vue/demos/#/material/grid/remote-data.html 
+
+## Project pre-requisites
+Make sure that you have the compatible versions of Node and Vue-Cli in your machine before starting to work on this project.
+
+## How to run this application?
+To run this application, you need to first clone the `binding-json-data-and-service-urls-to-the-vue-grid` repository and then navigate to its appropriate path where it has been located in your system.
+
+To do so, open the command prompt and run the below commands one after the other.
+
+```
+git clone https://github.com/SyncfusionExamples/binding-json-data-and-service-urls-to-the-vue-grid grid-example
+cd grid-example
+```
+
+## Installing
+Once done with downloading, next you need to install the necessary packages required to run this application locally. The `npm install` command will install all the needed Vue packages into your current project and to do so, run the below command.
+
+```
+npm install
+```
+
+## Running on development server
+Run `npm run serve` command for a dev server. Navigate to `http://localhost:8080/`. The app will automatically reload if you change any of the source files.
+
+## Further help
+
+To get more help on the vue CLI use go check out the [Vue-Cli README](https://github.com/vuejs/vue-cli/blob/master/README.md).
---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <!-- Uncomment this code to render button on the page -->
+    <!-- <ejs-button v-on:click="buttonClick">Load Data</ejs-button> -->
+    
+    <!-- Remove dataSource property and add ref="datagrid" in the below grid code while working with Ajax -->
+    <ejs-grid :dataSource="data">
+      <e-columns>
+        <e-column field="OrderID" headerText="Order ID" textAlign="Right" width="90"></e-column>
+        <e-column field="CustomerID" headerText="Customer ID" width="120"></e-column>
+        <e-column field="OrderDate" headerText="Order Date" textAlign="Right" format="yMd" type="date" width="120"></e-column>
+        <e-column field="ShipCountry" headerText="Ship Country" width="120"></e-column>
+      </e-columns>
+    </ejs-grid>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import { GridPlugin } from '@syncfusion/ej2-vue-grids';
+import { DataManager, WebApiAdaptor } from '@syncfusion/ej2-data';
+
+// Uncomment this code to bind data in Vue Grid through AJAX post
+// import { Ajax } from '@syncfusion/ej2-base';
+
+Vue.use(GridPlugin);
+
+export default {
+  data () {
+    return {
+       data: new DataManager({
+         url: "https://ej2services.syncfusion.com/production/web-services/api/Orders?$top=7",
+         adaptor: new WebApiAdaptor()
+       })
+    }
+  },
+  // Uncomment this code to bind data in Vue Grid through AJAX post
+  // methods: {
+  //   buttonClick: function() {
+  //     var grid = this.$refs.datagrid // Grid instance
+  //     var ajax = new Ajax(
+  //       "https://ej2services.syncfusion.com/production/web-services/api/Orders",
+  //       "GET"
+  //     );
+  //     ajax.send();
+  //     ajax.onSuccess = function(result) {
+  //     grid.ej2Instances.setProperties({dataSource : JSON.parse(result)})
+  //     };
+  //   }
+  // }
+}
+</script>
+
+<style>
+  @import url("https://cdn.syncfusion.com/ej2/material.css");
+</style>
